3fa9e486c8e4ba597ee288bf76f33a9593aa2bab
Andrew Rob <andrew-c-rob@live.com>
2018-05-15 13:17:22 -0500

    Implemented extend for underbar part 2

--- underbar.js
+++ underbar.js
@@ -1,14 +1,12 @@
 (function() {
   'use strict';
-
   window._ = {};
-
   // Returns whatever value is passed as the argument. This function doesn't
   // seem very useful, but remember it--if a function needs to provide an
   // iterator when the user does not pass one in, this will be handy.
-  _.identity = function(val) {
+    _.identity = function(val) {
     /* START SOLUTION */
-
+    return val;
     /* END SOLUTION */
   };
 
@@ -34,16 +32,19 @@
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
-    /* START SOLUTION */
-
-    /* END SOLUTION */
+    return n === undefined ? array[0] : array.slice(0, n);
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
   // last element.
   _.last = function(array, n) {
     /* START SOLUTION */
-
+    // When the input n not is provided, we return a single value from the array,
+    // rather than an array of values
+    if (n === undefined) {
+      return array[array.length - 1];
+    }
+    return array.slice(Math.max(0, array.length - n));
     /* END SOLUTION */
   };
 
@@ -54,7 +55,15 @@
   // iterator function over each item in the input collection.
   _.each = function(collection, iterator) {
     /* START SOLUTION */
-
+    if (Array.isArray(collection)) {
+      for (var i = 0; i < collection.length; i++) {
+        iterator(collection[i], i, collection);
+      }
+    } else {
+      for (var prop in collection) {
+        iterator(collection[prop], prop, collection);
+      }
+    }
     /* END SOLUTION */
   };
 
@@ -64,15 +73,27 @@
     // TIP: Here's an example of a function that needs to iterate, which we've
     // implemented for you. Instead of using a standard `for` loop, though,
     // it uses the iteration helper `each`, which you will need to write.
-    /* START SOLUTION */
+    var result = -1;
 
-    /* END SOLUTION */
+    _.each(array, function(item, index) {
+      if (item === target && result === -1) {
+        result = index;
+      }
+    });
+
+    return result;
   };
 
   // Return all elements of an array that pass a truth test.
   _.filter = function(collection, test) {
     /* START SOLUTION */
+    var result = [];
 
+    _.each(collection, function(val) {
+      test(val) && result.push(val);
+    });
+
+    return result;
     /* END SOLUTION */
   };
 
@@ -81,14 +102,29 @@
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
     /* START SOLUTION */
-
+    return _.filter(collection, function(val) {
+      return !test(val);
+    });
     /* END SOLUTION */
   };
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array, isSorted, iterator) {
     /* START SOLUTION */
+    var hash = {};
 
+    iterator = (isSorted && iterator) || _.identity;
+
+    _.each(array, function(val) {
+      var transformed = iterator(val);
+      if (hash[transformed] === undefined) {
+        hash[transformed] = val;
+      }
+    });
+
+    return _.map(hash, function(value) {
+      return value;
+    });
     /* END SOLUTION */
   };
 
@@ -99,7 +135,13 @@
     // like each(), but in addition to running the operation on all
     // the members, it also maintains an array of results.
     /* START SOLUTION */
+    var results = [];
 
+    _.each(collection, function(item, index, collection) {
+      results.push(iterator(item, index, collection));
+    });
+
+    return results;
     /* END SOLUTION */
   };
 
@@ -113,9 +155,12 @@
   // a certain property in it. E.g. take an array of people and return
   // an array of just their ages
   _.pluck = function(collection, key) {
-    /* START SOLUTION */
-
-    /* END SOLUTION */
+    // TIP: map is really handy when you want to transform an array of
+    // values into a new array of values. _.pluck() is solved for you
+    // as an example of this.
+    return _.map(collection, function(item){
+      return item[key];
+    });
   };
 
   // Reduces an array or object to a single value by repetitively calling
@@ -140,34 +185,78 @@
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
     /* START SOLUTION */
+    var initializing = arguments.length === 2;
 
+    _.each(collection, function(val) {
+      if (initializing) {
+        initializing = false;
+        accumulator = val;
+      } else {
+        accumulator = iterator(accumulator, val);
+      }
+    });
+
+    return accumulator;
     /* END SOLUTION */
   };
 
   // Determine if the array or object contains a given value (using `===`).
+  //inputs: 1 collection (array or object), target
+  //outputs: 1 boolean
+  //constraints: n/a
+  //edge cases: empty collection, or a collection that is not an array or object
+  //transformations: collection = ['book', 'magazine', 'newspaper'], target = 'newspaper' is target in collection? => true
+  //collection = {'item1': 'ball', 'item2': 'rope', 'item3': 'slide'}, traget = 'swing' is target in collection => false
   _.contains = function(collection, target) {
     // TIP: Many iteration problems can be most easily expressed in
     // terms of reduce(). Here's a freebie to demonstrate!
     /* START SOLUTION */
-
+    for (var i in collection) {
+      if (target === collection[i]) {
+        return true;
+      }
+    }
+    return false;
     /* END SOLUTION */
   };
 
 
   // Determine whether all of the elements match a truth test.
+  //inputs: collection (array, object), a function (iterator)
+  //outputs: 1 boolean
+  //constraints: n/a
+  //edge cases: empty array or object, what if the collection is neither an object or array
+  //transformation: collection = [2, 4, 6, 8], iterator = function(num) {return num % 2 === 0;} => true
+  //collection = [2, 4, 5, 6, 8], iterator = function(num) {return num % 2 === 0;} => false
   _.every = function(collection, iterator) {
     // TIP: Try re-using reduce() here.
     /* START SOLUTION */
-
+    var output = [];
+    var result = true;
+    iterator = iterator || _.identity;
+    _.each(collection, function(item) {
+      output.push(iterator(item));
+    });
+    _.each(output, function(element) {
+      if (!element) {
+        result = false;
+      }
+    });
+    return result;
     /* END SOLUTION */
   };
 
   // Determine whether any of the elements pass a truth test. If no iterator is
   // provided, provide a default one
+  //transformations: collection = [2, 4, 5, 6, 8], iterator = function(num) {return num % 2 === 0;} => true
+  //collection = [1, 3, 5, 7, 9], iterator = function(num) {return num % 2 === 0;} => false
   _.some = function(collection, iterator) {
     // TIP: There's a very clever way to re-use every() here.
     /* START SOLUTION */
-
+    iterator = iterator || _.identity;
+    return !_.every(collection, function(element) {
+        return !iterator(element);
+    });
     /* END SOLUTION */
   };
 
@@ -190,6 +279,11 @@
   //   }, {
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
+  //inputs: 1 object is passed in, second or third arguments are objects if passed in
+  //outputs: 1 object
+  //constraints: n/a
+  //edge cases: if the name of the key is all the same should it add the same key name as a different property or add an extra value into the same one key
+  //_.extend({'food1': 'muffin'}, {'food2': 'croissant', 'food3: 'danish'}) => {'food1': 'muffin', 'food2': 'croissant', 'food3': 'danish'}
   _.extend = function(obj) {
     /* START SOLUTION */
 
